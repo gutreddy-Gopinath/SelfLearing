197e87d73efe73cc3a31778162d6c912bf1868a2
gutreddy-Gopinath <63899151+gutreddy-Gopinath@users.noreply.github.com>
2020-04-18 13:13:27 +0530

    Create MulitpicatinWithNestLoopWithJava.java

---
+++ MulitpicatinWithNestLoopWithJava.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main{
+   // public static Scanner sc;
+    private static Scanner sc;
+    
+    public static void main(String[] args) {
+        int i,j;
+        System.out.println("Please enter the number: ");
+        sc = new Scanner(System.in);
+        //i=sc.nextInt();
+        for(i=sc.nextInt();i<=10; i++){
+            System.out.format("\n The Multiplication Table of %d is \n",i);
+            for(j=1;j<=10;j++){
+            
+                System.out.format("%d * %d = %d\n",i,j,i*j);
+            }
+        }
+        
+        
+        System.out.println();
+        
+        //for(i=sc.nextInt();i)
+        
+        
+    }
+}
