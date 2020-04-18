430620764c5525fd6884d5fc78733d73af9e09da
gutreddy-Gopinath <63899151+gutreddy-Gopinath@users.noreply.github.com>
2020-04-18 14:40:46 +0530

    Create PrintArrayElementsBasicswithJava.java

---
+++ PrintArrayElementsBasicswithJava.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class PrintArrayElementsBasicswithJava{
+    public static void main(String args[]){
+        int i;
+        int[] arr = new int[2];
+        arr[0] = 10;
+        arr[1] = 11;
+        for(i=0;i<=1;i++)
+        System.out.println(arr[i]);
+        
+    }
+    
+}
